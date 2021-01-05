4b672302f8a868c70ba99b142f75a800c6826b1a
TheMurkovich <70774296+TheMurkovich@users.noreply.github.com>
2021-01-05 17:00:15 +0300

    Обновлена команда add-money и remove-money.

--- cogs/addmoney.py
+++ cogs/addmoney.py
@@ -14,10 +14,10 @@ class Add(commands.Cog):
         self.collection = self.db["CherryCollection"]
 
     @commands.command(name="addmoney")
-    async def addmoney(self, ctx, arg1, arg2):
+    async def addmoney(self, ctx, arg1: discord.Member, arg2):
         if ctx.author.id == self.collection.find_one("settings")["owner_id"]:
-            bal = self.collection.users.find_one({"id": int(arg1)})["cash"]
-            self.collection.users.update_one({"id": int(arg1)}, {'$set': {"cash": bal + int(arg2)}})
+            bal = self.collection.users.find_one({"id": int(arg1.id)})["cash"]
+            self.collection.users.update_one({"id": int(arg1.id)}, {'$set': {"cash": bal + int(arg2)}})
             await ctx.send(embed=discord.Embed(title="Добавление денег",
                                                description=f"**Успешно добавлено __{arg2}__ <:infiniticoin:777091801453690910> пользователю __{arg1}__**",
                                                colour=discord.Colour.green()))
